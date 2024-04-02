a7ceec90ed4b2156592c37262c6eca91382b4eb7
SASTA HARI HARAN R <133386882+Sastahariharan567@users.noreply.github.com>
2024-04-02 20:20:03 +0530

    Update App.js

--- LITTLE LEMON/src/App.js
+++ LITTLE LEMON/src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 
 function App() {
   return (
- 
+   <>
     <div>
       <h1>Meta Frontend Developer Capstone Starter Files</h1>
       <h2> Little Lemon </h2>
     </div>
+    </>
   );
 }
 
